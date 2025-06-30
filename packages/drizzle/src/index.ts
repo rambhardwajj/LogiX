3edd81ff49cc6095899a3913740f7a5cec480c68
rambhardwajj <rambhardwaj101000@gmail.com>
2025-06-30 21:51:28 +0530

    feat: added db connection, and utils exports

--- packages/drizzle/src/index.ts
+++ packages/drizzle/src/index.ts
@@ -1,2 +1,24 @@
-export * from "./db/schema"
+export * from "./db/schema";
 export * from "drizzle-orm";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { sql } from "drizzle-orm";
+import { logger } from "@repo/utils";
+import { env } from "@repo/zod";
+
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
+export const db = drizzle(pool);
+
+export const connectDrizzle = async () => {
+  try {
+    await db.execute(sql`SELECT 1`);
+    logger.info("Drizzle connected to the database");
+  } catch (error: any) {
+    logger.error("Drizzle failed to connect to the database", {
+      message: error.message,
+    });
+    process.exit(1);
+  }
+};
